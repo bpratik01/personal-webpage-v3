a07570d4a513ed5eb0882a67ce9d2f0b21f0db18
ash <a.y.deshmukh26@gmail.com>
2025-02-10 21:29:48 +0530

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/personal-webpage-v3/", // Ensure this matches your repo name
+  build: {
+    outDir: 'dist'
+  }
 })
