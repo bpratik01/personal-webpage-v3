97f0debac9bd54e8b65072e3f970471175f4d17b
almightypu5h <a.y.deshmukh26@gmail.com>
2025-03-25 10:24:50 +0530

    resolved error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Home } from './pages/Home';
 import { Blog } from './pages/Blog';
